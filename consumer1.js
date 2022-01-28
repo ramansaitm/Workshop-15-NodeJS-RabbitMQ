a197de82066b925e2bc2bc46aafababaffd8dd84
Raman kumar <92011450+ramansaitm@users.noreply.github.com>
2022-01-28 23:58:39 +0530

    first commit

---
+++ consumer1.js
@@ -0,0 +1,24 @@
+const amqp = require("amqplib");
+
+connect();
+
+async function connect() {
+    try {
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+        channel.consume("jobs", message =>{
+         const input =JSON.parse(message.content.toString())
+         console.log(`the message :${input.number}`)
+         if(input.number==7){
+             channel.ack(message)
+         }
+        })
+        console.log("Waiting for messages ..");
+
+    
+}
+    catch (ex) {
+        console.log(ex);
+    }
+}
